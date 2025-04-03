ba0db50dd22b9074d2261047f6405224a3ef62b7
Max-Lev <maxlevtov@gmail.com>
2025-04-03 22:59:22 +0300

    active tab

--- src/app/components/tabs-container/tabs-container.component.ts
+++ src/app/components/tabs-container/tabs-container.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
 import { TABS_CONFIG } from './config';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-tabs-container',
@@ -13,13 +14,36 @@ import { TABS_CONFIG } from './config';
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.scss'
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements OnInit,OnDestroy {
+  
   links = TABS_CONFIG.links;
   activeLink = TABS_CONFIG.activeLink
   background = TABS_CONFIG.background;
 
-  ngOnInit(): void {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  destroy$ = new Subject<void>();
 
+  ngOnInit(): void {
+    this.setActiveTab();
+  }
+
+  setActiveTab() {
+    this.router.events.pipe(takeUntil(this.destroy$)).subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.activatedRoute.root;
+        let child = currentRoute.firstChild;
+        if (child && child.snapshot.url.length) {
+          const index = this.links.findIndex(link => link.toLocaleLowerCase() === child.snapshot.url[0].path);
+          this.activeLink = this.links[index];
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
