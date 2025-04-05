be148b8b73696b7453f24b4ce91c4723fa4b0560
Max-Lev <maxlevtov@gmail.com>
2025-04-05 13:46:45 +0300

    CustomRouteReuseStrategy && album

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { GetBreedsResolver } from './core/resolvers/get-breeds.resolver';
 export const routes: Routes = [
     {
         path: 'search', component: SearchComponent,
-        resolve:{
-            breeds:GetBreedsResolver
-        }
+        resolve: {
+            breeds: GetBreedsResolver
+        },
+        data: { reuseComponent: true }
     },
     {
-        path: 'adopt', loadComponent:()=> 
-            import('./components/adoption/adoption.component').then(m=>m.AdoptionComponent)
+        path: 'adopt', loadComponent: () =>
+            import('./components/adoption/adoption.component').then(m => m.AdoptionComponent),
+        data: { reuseComponent: true }
     },
     {
         path: '', redirectTo: 'search', pathMatch: 'full'
--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { IAlbum } from '../../core/models/breeds.model';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-album',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    CommonModule
   ],
   templateUrl: './album.component.html',
   styleUrl: './album.component.scss'
